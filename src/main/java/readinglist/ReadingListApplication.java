635e70fe24134df86a3467823f14e642206465d8
yinxunyang <yinxunyang@bestvike.com>
2019-06-19 15:19:56 +0800

    添加controller

--- src/main/java/readinglist/ReadingListApplication.java
+++ src/main/java/readinglist/ReadingListApplication.java
@@ -6,15 +6,16 @@ import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
-public class ReadingListApplication /*extends WebMvcConfigurerAdapter*/ {
+public class ReadingListApplication extends WebMvcConfigurerAdapter {
 
     public static void main(String[] args) {
         SpringApplication.run(ReadingListApplication.class, args);
     }
-    
- /*   @Override
+    //那么通过下面的配置，不用添加LoginController或者处理“readingList”的方法
+    // 就可以直接通过“http://localhost:8080/readingList”访问到readingList.html页面了！
+    @Override
     public void addViewControllers(ViewControllerRegistry registry) {
-      registry.addRedirectViewController("/", "/readingList");
-    }*/
-    
+        registry.addRedirectViewController("/", "/readingList");
+    }
+
 }
