5a6d9720382c6150a9062818454d8a2b201fa78c
yinxunyang <yinxunyang@bestvike.com>
2019-06-20 10:46:21 +0800

    自定义SecurityConfig

---
+++ src/main/java/readinglist/SecurityConfig.java
@@ -0,0 +1,44 @@
+package readinglist;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private ReaderRepository readerRepository;
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // 要求登陆者有READER角色
+        http
+                .authorizeRequests()
+                .antMatchers("/").access("hasRole('READER')")
+                .antMatchers("/**").permitAll()
+                .and()
+                .formLogin()
+                .loginPage("/readingList") // 设置登录表单的路径
+                .failureUrl("/readingList?error=true");
+
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(new UserDetailsService() {
+            @Override
+            public UserDetails loadUserByUsername(String user) throws UsernameNotFoundException {
+                // 可以自定义数据库查询
+                return readerRepository.findOne(user);
+            }
+        });
+    }
+}
