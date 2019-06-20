44a3f09d5daccc61e9a3174f5a9797d4f137287c
yinxunyang <yinxunyang@bestvike.com>
2019-06-20 09:52:36 +0800

    添加security依赖

---
+++ src/test/java/BaseTest.java
@@ -0,0 +1,8 @@
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import readinglist.ReadingListApplication;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = ReadingListApplication.class)
+public class BaseTest {
+}
---
+++ src/test/java/TestGradle.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class TestGradle extends BaseTest{
+
+    @Test
+    public void test1(){
+
+    }
+}
